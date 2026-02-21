03ab07d1923c8dae59c1533be82e85a735ec7e82
Abhishek Goyal <goyalabhishek044@gmail.com>
2026-02-21 09:02:49 +0530

    prime commit

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 # Project : Prime-check + Factorial
-# Authour : 
+# Authour : Abhishek
 # This code checks if a number is prime or not and calculate the factorial of the number
+def prime_check(n):
+    if n<=1:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
