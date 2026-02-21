d68f478568fdf102f5da576a162462cfe845aebd
Abhishek Goyal <goyalabhishek044@gmail.com>
2026-02-21 09:04:57 +0530

    fact commit

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 # Project : Prime-check + Factorial
-# Authour : 
+# Authour : Rudra
 # This code checks if a number is prime or not and calculate the factorial of the number
+def fact(n):
+    res=1
+    for i in range(1,n+1):
+        result*=i
+    return res
